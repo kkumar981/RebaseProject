eebb1f2194b06753d9fe4096dae63b56262ad5cc
Rakesh Kumar <kkumar981@gmail.com>
2018-03-07 20:16:32 +0530

    localbranch Commit

--- GitRebase/src/com/rebase/test/TestSample.java
+++ GitRebase/src/com/rebase/test/TestSample.java
@@ -3,7 +3,7 @@ package com.rebase.test;
 public class TestSample {
 
 	public static void main(String[] args) {
-		System.out.println("test12");
+		System.out.println("FUTURE BRANCH ");
 		System.out.println("hi");
 		System.out.println("Localcommit");
 	}
